ad337e89a3875eb32a4df7b534b0c56a6323d1ef
mustafaduyarer <mustafaduyarer@gmail.com>
2024-06-19 08:47:54 +0200

    yorum eklendi

---
+++ Create a Capitalize Func.js
@@ -0,0 +1,13 @@
+const toCapital = str => {
+    const words = str.split(" ");
+    return words.map(word => word[0].toUpperCase() + word.slice(1)).join(" ");
+  };
+  
+  const shortStr = "Hi there";
+  toCapital(shortStr); // Hi There
+  console.log(toCapital(shortStr));
+  
+  const longStr = "the quick brown fox jumped over the lazy dog";
+  toCapital(longStr); // The Quick Brown Fox Jumped Over The Lazy Dog
+  console.log(toCapital(longStr));
+  //Yorum ekle
